ec7ed424895740a1d041ca93d184bb27881cb447
Dominik Sieroń <domin.powtorki@gmail.com>
2024-04-05 12:53:57 +0200

    Feat: Lab 3 and 4

--- l4/src/main.cpp
+++ l4/src/main.cpp
@@ -1,31 +1,59 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-#define _USE_MATH_DEFINES
-
 #include <iostream>
 #include <math.h>
 
 const GLchar *vertexShaderSource =
     "#version 330 core\n"
     "layout(location = 0) in vec3 position;\n"
+    "uniform float scrollOffset;\n"
     "out vec3 vertexColor;\n"
     "void main()\n"
     "{\n"
-    " gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+    " gl_Position = vec4(position.x, position.y+scrollOffset, position.z, 1.0);\n"
     " vertexColor = vec3(0.30f, 0.50f, 1.0f);\n"
     "}\0";
 
 const GLchar *fragmentShaderSource =
     "#version 330 core\n"
     "out vec4 fragmentColor;\n"
+    "uniform vec3 shapeColor;\n"
     "void main()\n"
     "{\n"
-    " fragmentColor = vec4(0.30f, 0.50f, 1.0f, 1.0f);\n"
+    " fragmentColor = vec4(shapeColor, 1.0f);\n"
     "}\0";
 
-GLfloat *vertices;
-GLuint *indices;
+float scrollOffset = 0.0f;
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
+  scrollOffset += yoffset * 0.1f;
+  if (scrollOffset > 0.5f) {
+    scrollOffset = 0.5f;
+  } else if (scrollOffset < -0.5f) {
+    scrollOffset = -0.5f;
+  }
+}
+
+GLfloat colors[][3] = {
+    {0.5f, 0.0f, 0.0f},
+    {0.3f, 0.1f, 0.0f},
+    {0.2f, 0.0f, 1.0f}
+};
+
+int colorIndex = 0;
+
+void keyPressPooling(GLFWwindow* window){
+    if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS) {
+        colorIndex = 0;
+    }
+    if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS) {
+        colorIndex = 1;
+    }
+    if (glfwGetKey(window, GLFW_KEY_3) == GLFW_PRESS) {
+        colorIndex = 2;
+    }
+}
 
 int main() {
   // inicjalizacja GLFW
@@ -35,8 +63,8 @@ int main() {
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
   // Tworzenie okna
-  const unsigned int window_width = 800;
-  const unsigned int window_height = 800;
+  const unsigned int window_width = 1000;
+  const unsigned int window_height = 1000;
   GLFWwindow *window = glfwCreateWindow(window_width, window_height,
                                         "grafika komputerowa", NULL, NULL);
   if (window == NULL) {
@@ -44,7 +72,9 @@ int main() {
     glfwTerminate();
     return -1;
   }
+
   glfwMakeContextCurrent(window);
+  glfwSetScrollCallback(window, scroll_callback);
 
   // inicjalizacja GLAD
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
@@ -87,53 +117,41 @@ int main() {
   glDetachShader(shaderProgram, vertexShader);
   glDetachShader(shaderProgram, fragmentShader);
 
-  int n = 0;
-  while (n < 8) {
-    std::cout << "Podaj ilosc wierzcholkow wielokata: ";
-    std::cin >> n;
-    std::cout << "\n";
-  }
+  GLfloat vertices[] = {
+    -0.5f, -0.5f, 0.0f, // Vertex 0 
+     0.0f,  0.5f, 0.0f, // Vertex 1 
+    -0.1f,  0.0f, 0.0f, // Vertex 2 
+     -0.7f,  -0.3f, 0.0f, // Vertex 3 
+    0.6f, 0.6f, 0.0f, // Vertex 4 
+     0.1f,  0.6f, 0.0f, // Vertex 5 
+    0.2f,  0.1f, 0.0f, // Vertex 6 
+     0.8f,  0.4f, 0.0f, // Vertex 7 
+  };
 
-  vertices = new GLfloat[(n + 1) * 3];
-  vertices[0] = 0.0f;
-  vertices[1] = 0.0f;
-  vertices[2] = 0.0f;
+  GLuint indices[] = {
+    0, 1, 2, // The first triangle uses vertices 0, 1, and 2
+    0, 1, 3, // The second triangle uses vertices 0, 1, and 3
+    4, 5, 6, // 
+    4, 5, 7, // 
+  };
 
-  for (int i = 0; i <= n; i++) {
-    vertices[(i + 1) * 3] = 0.5f * std::cos(2 * M_PI * i / n);
-    vertices[(i + 1) * 3 + 1] = 0.5f * std::sin(2 * M_PI * i / n);
-    vertices[(i + 1) * 3 + 2] = 0.0f;
-  }
-
-  indices = new GLuint[(n + 1) * 3];
-  for (int i = 1; i <= n; i++) {
-    indices[i * 3 + 0] = 0;
-    indices[i * 3 + 1] = i;
-    indices[i * 3 + 2] = i + 1;
-  }
-  indices[n * 3] = 0;
-  indices[n * 3 + 1] = n;
-  indices[n * 3 + 2] = 1;
 
   GLuint VAO;
   glGenVertexArrays(1, &VAO);
   glBindVertexArray(VAO);
 
+
   GLuint VBO;
   glGenBuffers(1, &VBO);
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  glBufferData(GL_ARRAY_BUFFER, (n + 1) * 3 * sizeof(GLfloat), vertices,
-               GL_STATIC_DRAW);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-  delete[] vertices;
-  
+
   GLuint EBO;
   glGenBuffers(1, &EBO);
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, (n + 1) * 3 * sizeof(GLuint), indices,
-               GL_STATIC_DRAW);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-  delete[] indices;
 
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat),
                         (void *)0);
@@ -143,14 +161,28 @@ int main() {
 
   glViewport(0, 0, (GLuint)window_width, (GLuint)window_height);
 
+
+  int scrollOffsetLocation = glGetUniformLocation(shaderProgram, "scrollOffset");
+  int colorLocation = glGetUniformLocation(shaderProgram, "shapeColor");
+  GLfloat defaultColor[] = {0.0f, 1.0f, 0.0f};
+
   // pętla zdarzeń
   while (!glfwWindowShouldClose(window)) {
+    keyPressPooling(window);
+
     glClearColor(0.18f, 0.2f, 0.22f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
     glUseProgram(shaderProgram);
+
+    glUniform1f(scrollOffsetLocation, scrollOffset);
+    glUniform3fv(colorLocation, 1, defaultColor);
+
     glBindVertexArray(VAO);
-    glDrawElements(GL_TRIANGLES, (n + 1) * 3, GL_UNSIGNED_INT, 0);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glUniform3fv(colorLocation, 1, colors[colorIndex]);
+    glUniform1f(scrollOffsetLocation, 0);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)(6 * sizeof(GLuint))); 
     glBindVertexArray(0);
 
     glfwSwapBuffers(window);
